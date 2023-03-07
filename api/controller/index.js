5bf96cb5607f0f23f1a9a1c0701c0cc7348967e0
Meezaan Davids <meezaandavids365@gamil.com>
2023-03-07 10:23:54 +0200

    api-setup

---
+++ api/controller/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const path = require('path');
+
+const bodyParser = require('body-parser')
+
+const route = express.Router(); 
+
+
+
+
+module.exports = route;
