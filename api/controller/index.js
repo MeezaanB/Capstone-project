6782cdb462282da1740d5fe223be922c79e2aad6
Meezaan Davids <meezaandavids365@gamil.com>
2023-03-08 11:40:50 +0200

    controller

--- api/controller/index.js
+++ api/controller/index.js
@@ -2,11 +2,93 @@ const express = require('express')
 
 const path = require('path');
 
-const bodyParser = require('body-parser')
+const parser = require('body-parser')
 
-const route = express.Router(); 
+const router = express.Router();
+
+const {User, Product} = require('../model')
+
+const user = new User();
+
+const product = new Product();
+
+router.get('/', (req, res)=> {
+    res.status(200).sendFile(path.join(__dirname, '../view/index.html'))
+})
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Register User
+
+register.post('/login', parser.json(), (req, res) => {
+    user.createUser(req, res);
+})
+
+// Login 
+
+router.post('/login', parser.join(), (req, res) => {
+    user.login(req, res);
+})
+
+// View all users 
+
+router.get('/users', (req, res) => {
+    user.fetchUsers(req, res);
+})
+
+// View single user
+
+router.get('/user/:id', (req, res) => {
+    user.fetchUser(req, res);
+})
+
+// Update user
+
+router.put('/user/:id', parser.json(), (req, res) => {
+    user.updaterUser(req, res);
+})
+
+// Delete user 
+
+router.delete('/user/:id', (req, res) => {
+    user.deleteUser(req, res);
+})
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Create a new product
+
+register.post('/product', parser.json(), (req, res) => {
+    product.addProduct(req, res);
+})
+
+// View all products
+
+router.get('/products', (req, res) => {
+    user.fetchProducts(req, res);
+})
+
+// View single product
+
+router.get('/product/:id', (req, res) => {
+    product.fetchProduct(req, res);
+})
+
+// Update product
+
+router.put('/product/:id', parser.json(), (req, res) => {
+    product.updateProduct(req, res);
+})
+
+// Delete product
+
+router.delete('/product/:id', (req, res) => {
+    product.deleteProduct(req, res);
+})
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
-
-module.exports = route;
+module.exports = router;
